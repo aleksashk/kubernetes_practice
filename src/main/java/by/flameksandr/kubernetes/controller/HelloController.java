ceed6248a62cea35d94ec51cd3f78d36facda5fb
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-08-07 16:33:43 +0300

    refactoring HelloController

--- src/main/java/by/flameksandr/kubernetes/controller/HelloController.java
+++ src/main/java/by/flameksandr/kubernetes/controller/HelloController.java
@@ -1,4 +1,4 @@
-package by.flameksandr.kubernetes;
+package by.flameksandr.kubernetes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
